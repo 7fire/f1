9d780d754d6dde75fb5508eb4e7eaf5384be9ea3
LSH <LSH>
2017-06-09 20:35:18 +0800

    Initial commit

---
+++ src/com/qf/day/UserDao.java
@@ -0,0 +1,10 @@
+package com.qf.day;
+
+/**
+ * Created by Administrator on 2017/6/9 0009.
+ */
+public class UserDao {
+    public static void main(String[] args) {
+        System.out.println("哈哈哈哈哈");
+    }
+}
