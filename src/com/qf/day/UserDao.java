c23b4e4239c688c40e9011a0474f04ba838385cb
7fire <lshlmm1314>
2017-06-09 20:41:54 +0800

    牛牛

--- src/com/qf/day/UserDao.java
+++ src/com/qf/day/UserDao.java
@@ -7,5 +7,6 @@ public class UserDao {
     public static void main(String[] args) {
         System.out.println("哈哈哈哈哈");
         System.out.println("嘿嘿嘿嘿嘿");
+        System.out.println("牛俊东大帅哥");
     }
 }
